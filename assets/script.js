ff1716f057aaba526c02f3f039b860695021fda7
David Shober <shobes1421@hotmail.com>
2020-03-29 11:15:46 -0600

    Added first time block with save button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
-//Adding todays date to the jumbotron
+//Adding todays date to the jumbotron 
+//Provided by w3schools ref: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
 var todayDate = new Date()
 document.getElementById("todaysDate").innerHTML = todayDate;
