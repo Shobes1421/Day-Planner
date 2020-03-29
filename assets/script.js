81415bb5e34ed940f84bfca2c2a5551a78e2c9d1
David Shober <shobes1421@hotmail.com>
2020-03-29 12:02:34 -0600

    Attempting to save text to local storage function

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,13 @@
 //Provided by w3schools ref: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
 var todayDate = new Date()
 document.getElementById("todaysDate").innerHTML = todayDate;
+
+//function that when the user inputs information
+//it will save that infromation to the local storage.
+
+function renderText() {
+    var saveText9 = JSON.parse(localStorage.getItem("9:00 am"));
+    $("#9").val("");
+    $("#9").val(saveText9);
+
+}
