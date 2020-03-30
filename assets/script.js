43aced6a37b6c31de5b0f3158279b388b12fd3e3
David Shober <shobes1421@hotmail.com>
2020-03-29 20:20:22 -0600

    Added style to day planner

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,6 @@
-//Adding todays date to the jumbotron 
-//Provided by w3schools ref: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
-var todayDate = new Date()
+
+var todayDate= new Date();
 document.getElementById("todaysDate").innerHTML = todayDate;
-
-
 
 
 
@@ -19,42 +16,43 @@ function renderText() {
     $("#9").val(saveText9);
     
     //10:00 time block localstorage save
-    var saveText10 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText10 = JSON.parse(localStorage.getItem("10:00 am"));
     $("#10").val("");
     $("#10").val(saveText10);
 
     //11:00 time block localstorage save
-    var saveText11 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText11 = JSON.parse(localStorage.getItem("11:00 am"));
     $("#11").val("");
     $("#11").val(saveText11);
 
     //12:00 time block localstorage save
-    var saveText12 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText12 = JSON.parse(localStorage.getItem("12:00 pm"));
     $("#12").val("");
     $("#12").val(saveText12);
 
     //1:00 time block  localstorage save
-    var saveText1 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText1 = JSON.parse(localStorage.getItem("1:00 pm"));
     $("#13").val("");
     $("#13").val(saveText1);
 
     //2:00 time block localstorage save
-    var saveText2 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText2 = JSON.parse(localStorage.getItem("2:00 pm"));
     $("#14").val("");
     $("#14").val(saveText2);
 
     //3:00 time block localstorage save
-    var saveText3 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText3 = JSON.parse(localStorage.getItem("3:00 pm"));
     $("#15").val("");
     $("#15").val(saveText3);
 
     //4:00 time block localstorage save
-    var saveText4 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText4 = JSON.parse(localStorage.getItem("4:00 pm"));
     $("#16").val("");
     $("#16").val(saveText4);
 
     //5:00 time block localstorage save
-    var saveText5 = JSON.parse(localStorage.getItem("9:00 am"));
+    var saveText5 = JSON.parse(localStorage.getItem("5:00 pm"));
     $("#17").val("");
     $("#17").val(saveText5);
 }
+
